2ce9edc411e7e894c4fd2ddddf190d1f1112459e
Oscarmacieira <macieiraoscar@icloud.com>
2024-09-27 22:26:01 +0100

    fix(tailwind.config.ts): src files in content

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -62,4 +63,5 @@ const config: Config = {
   },
   plugins: [tailwindCSSAnimatePlugin],
 };
+
 export default config;
