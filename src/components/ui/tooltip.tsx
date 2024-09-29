a7bb8fcd4a4cacd1cbb4b39c744016f88d8bf45b
Oscarmacieira <macieiraoscar@icloud.com>
2024-09-29 23:49:57 +0100

    feat(ui): button ghost variant & tooltip

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -19,7 +19,12 @@ const TooltipContent = React.forwardRef<
     ref={ref}
     sideOffset={sideOffset}
     className={cn(
-      "z-50 overflow-hidden rounded-md bg-primary px-3 py-1.5 text-xs text-primary-foreground animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+      "z-50 overflow-hidden border border-background rounded-md bg-primary px-3 py-1.5 text-xs text-primary-foreground",
+      "animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95",
+      "data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+      "shadow-lg shadow-primary/20 backdrop-blur-sm",
+      "before:content-[''] before:absolute before:-z-10 before:inset-0 before:bg-gradient-to-r before:from-primary/50 before:to-secondary/50 before:opacity-75 before:blur-xl",
+      "after:content-[''] after:absolute after:inset-0 after:bg-gradient-to-r after:from-primary/10 after:to-secondary/10 after:opacity-100",
       className
     )}
     {...props}
