02a1ccbad1718b79cf2572684d76f3a01b95bc07
Oscarmacieira <macieiraoscar@icloud.com>
2024-09-29 17:38:44 +0100

    feat(lib): added site-config

---
+++ src/lib/site-config.ts
@@ -0,0 +1,17 @@
+export const SITE_CONFIG = {
+  name: "oscarmac",
+  description:
+    "one more fullstack developer but a true enthousiast of defi & blockchain",
+  productionUrl: "https://oscarmac.xyz",
+  domain: "oscarmac.xyz",
+  appIcon: "/images/icon.png",
+  brand: {
+    primary: "#FF0000",
+  },
+  socials: {
+    linkedin: "https://www.linkedin.com/in/oscarmac/",
+    github: "https://github.com/oscarmacieira",
+    twitter: "https://x.com/_scarmac",
+    mail: "contact@oscarmac.xyz",
+  },
+};
