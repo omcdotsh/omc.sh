b686667acfeb2bc1c4ddde2f2c42f0551390dfbb
Oscarmacieira <macieiraoscar@icloud.com>
2024-10-24 12:10:09 +0100

    feat: added animated cursor

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,9 +3,16 @@
 import { ThemeProvider } from "next-themes";
 import { PropsWithChildren } from "react";
 
+import dynamic from "next/dynamic";
+
+const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
+  ssr: false,
+});
+
 export const Providers = ({ children }: PropsWithChildren) => {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+      <AnimatedCursor />
       {children}
     </ThemeProvider>
   );
